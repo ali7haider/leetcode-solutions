407b3673738c8f37d91469669f9bad259e39202d
Ali Haider <ali.haider@pentaloop.com>
2026-05-21 18:12:53 +0500

    Add implementation for Product of Array Except Self

--- javascriptProblemSol.js
+++ javascriptProblemSol.js
@@ -95,3 +95,25 @@ function hasDuplicate(arr){
     return false
 }
 console.log(hasDuplicate([1,2,3,5]))
+
+// Product of Array Except Self
+
+function productExceptSelf(nums){
+    const n=nums.length
+    const prefix = new Array(n).fill(1);
+    const suffix = new Array(n).fill(1);
+    const answer = new Array(n).fill(1);
+    
+    for(let i=1;i<n;i++){
+        prefix[i]=prefix[i-1]*nums[i-1]
+    }
+    for (let i=n-2;i>=0;i--){
+        suffix[i]=suffix[i+1]*nums[i+1]
+    }
+    for (let i=0;i<n;i++){
+        answer[i]=prefix[i]*suffix[i]
+    }
+    return answer;
+}
+
+console.log("Product Except Self",productExceptSelf([4,6,8,9]))
